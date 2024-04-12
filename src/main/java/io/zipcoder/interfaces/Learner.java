8e7387650580f135dd2de92026c9b5733c8b195c
Danny Cao <dannycao1997@yahoo.com>
2024-04-12 14:44:03 -0400

    commented the instructions from README

--- src/main/java/io/zipcoder/interfaces/Learner.java
+++ src/main/java/io/zipcoder/interfaces/Learner.java
@@ -6,3 +6,15 @@ public interface Learner {
 
     Double getTotalStudyTime();
 }
+
+/*
+Instructions : Part 2.0 - Create Learner Interface
+Create a Learner interface.
+Learner should declare method signature:
+Method name: learn
+Method parameters: double numberOfHours
+Method return-type: void
+Learner should declare method signature:
+Method name: getTotalStudyTime
+Method return-type: Double
+ */
