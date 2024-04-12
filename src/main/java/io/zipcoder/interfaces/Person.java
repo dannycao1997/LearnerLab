242b1743608e9a5094adb0818e1538db9bd381f3
Danny Cao <dannycao1997@yahoo.com>
2024-04-12 14:23:28 -0400

    writing code for TestPerson

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,6 +1,7 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+
     private final long id;
     private String name;
 
@@ -10,26 +11,16 @@ public class Person {
     }
 
     public long getId(){
-        return id; //
+        return id;
     }
+
 
     public String getName(){
         return name;
     }
 
-    public void setName(){
+    public void setName(String name){
         this.name = name;
     }
 }
 
-/*
-INSTRUCTIONS
-### Part 1.1 - Create `Person` Class
-* Create a `Person` class.
-	* The class should declare a `final` field named `id` of type `long`.
-	* The class should declare a field named `name` of type `String`.
-	* `Person` constructor should have a parameter of type `long` and `String` which sets the `id` and `name` field to the respective values.
-	* The class should define a `getId()` method which returns the `Person` object's `id` field.
-	* The class should define a `getName()` method which returns the `Person` object's `name` field.
-	* The class should define a `setName()` method which sets the `Person` object's `name` field.
- */
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,22 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    private Person person;
+
+    @Test
+    public void testConstructor(){
+        Person person = new Person(10, "Danny");
+    }
+
+    @Test
+    public void testSetName(){
+        Person person = new Person(10, "Danny");
+        person.setName("Beatriz");
+        Assert.assertEquals("Beatriz", person.getName());
+    }
 
 }
